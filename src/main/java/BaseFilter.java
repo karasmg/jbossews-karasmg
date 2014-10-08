f32b77c318cc3709128586fba139c6034f84f8ec
karasmg <m2880806799@gmail.com>
2014-10-09 00:39:32 +0300

    todays changes

---
+++ src/main/java/BaseFilter.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class BaseFilter
+ */
+@WebFilter(asyncSupported = true, urlPatterns = { "/BaseFilter" })
+public abstract class BaseFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public BaseFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		doFilter((HttpServletRequest) request, (HttpServletResponse) response);
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	public abstract void doFilter(HttpServletRequest request, HttpServletResponse response);
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
