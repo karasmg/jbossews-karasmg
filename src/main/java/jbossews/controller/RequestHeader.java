fad990d73bc18ad065c22b9f3fe396bf1f182106
karasmg <m2880806799@gmail.com>
2014-10-10 23:16:54 +0300

    users

--- src/main/java/jbossews/controller/RequestHeader.java
+++ src/main/java/jbossews/controller/RequestHeader.java
@@ -4,13 +4,21 @@ package jbossews.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import jbossews.model.DaoFactory;
+import jbossews.model.MySqlDataFactory;
+import jbossews.model.MySqlUserDao;
+import jbossews.model.User;
 
 /**
  * Servlet implementation class RequestHeader
@@ -62,12 +70,30 @@ public class RequestHeader extends HttpServlet {
         String name = (String)e.nextElement();
 
         String value = request.getHeader(name);
-        
+        MySqlDataFactory daoFactory = new MySqlDataFactory();
+        try {
+			Connection con = daoFactory.getConnection();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        List<User> users = null;
+		try {
+			users = new MySqlUserDao(null).getAll();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        out.println(users);
 	}
+       /*
         out.println("Аттрибуты:");
         out.println("method = "+request.getAttribute("method"));
         out.println("remoteAddr = "+request.getAttribute("remoteAddr"));
         out.println("queryString = "+request.getAttribute("queryString"));
         out.println("protocol = "+request.getAttribute("protocol"));
+        */
+        
+        
 	}
 }
