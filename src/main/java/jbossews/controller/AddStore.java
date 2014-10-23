a71055bb343d6629b7469fcb680610298eb9efc0
karasmg <m2880806799@gmail.com>
2014-10-23 16:26:15 +0300

    iu

--- src/main/java/jbossews/controller/AddStore.java
+++ src/main/java/jbossews/controller/AddStore.java
@@ -28,7 +28,7 @@ import jbossews.model.User;
  */
 @WebServlet("/addstore")
 public class AddStore extends HttpServlet {
-	private static final Logger log = Logger.getLogger(AddStore.class);
+	static final Logger log = LogManager.getLogger(AddStore.class.getName());
 	private static final long serialVersionUID = 1L;
 
 	/**
