d4e181298eca613d3880d2614c90c1d850c0a818
karasmg <m2880806799@gmail.com>
2014-10-23 16:15:48 +0300

    ui

--- src/main/java/jbossews/controller/AddStore.java
+++ src/main/java/jbossews/controller/AddStore.java
@@ -49,6 +49,7 @@ public class AddStore extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 
+		
 		//создаем объект магазин и заполняем его поля
 		Store newstore = new Store();
 		newstore.setName(request.getParameter("p1"));
